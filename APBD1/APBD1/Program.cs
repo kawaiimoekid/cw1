0a87385f96a514cd0370e63a56789c1c5a36a6ef
Jakub Kurchan <s15383@pjwstk.edu.pl>
2020-03-02 09:18:03 +0100

    Pierwszy commit

---
+++ APBD1/APBD1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace APBD1
+{
+    class Program
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Console.Write("URL: ");
+            var address = Console.ReadLine();
+
+            try
+            {
+                var response = await _httpClient.GetAsync(address);
+                var content = await response.Content.ReadAsStringAsync();
+
+                var matches = Regex.Matches(content, @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+
+                Console.WriteLine("Found addresses:");
+                foreach(var email in matches)
+                {
+                    Console.WriteLine(email);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
